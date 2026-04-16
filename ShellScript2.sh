731687547913d57c539b6e2a287422fda43c7428
ctnpatil14 <ctnpatil14@gmail.com>
2026-04-16 06:04:26 +0530

    shellscript2 add kela

---
+++ ShellScript2.sh
@@ -0,0 +1,24 @@
+switch case
+
+
+
+
+Looping statement 
+
+remove spaces
+sed -i '/^$/d' loops*
+
+#Generally for loop reads word by word in shell but if line has only one word
+
+#file1 
+#one two three
+#four five six
+
+#6loops 
+
+#file2 
+#one
+#two
+#three 
+#3loops 
+
